12967881c3e3a183bf9fac882360c1678d887fbc
Kristian Hentschel <khentschel@gmail.com>
2015-08-05 23:08:58 +0200

    Add MSN fixed width station record type and supporting fixed-widths reading methods to its superclass.

---
+++ src/com/kristianhentschel/transportexp/ingest/utilities/records/UnitedKingdomATOC/MasterStationNamesStationRecord.java
@@ -0,0 +1,35 @@
+package com.kristianhentschel.transportexp.ingest.utilities.records.UnitedKingdomATOC;
+
+import com.kristianhentschel.transportexp.ingest.utilities.records.FixedWidthRecord;
+
+/**
+ * Created by Kristian on 05/08/2015.
+ *
+ * Describes an "A" type station record in the .msn Master Station Names file
+ * as specified in the ATOC document RSPS5041 Version 06-00 (Section 3.2.7.2).
+ *
+ * The Station Details record occurs once for each TIPLOC and associates a station name
+ * and the station's 3-alpha codes with a TIPLOC, and also defines its location in terms
+ * of Easting and Northing.
+ */
+public class MasterStationNamesStationRecord extends FixedWidthRecord{
+    public MasterStationNamesStationRecord(String record_text) {
+        super(record_text);
+
+        setField("record_type", takeChars(1));
+        skipChars(4);
+        setField("station_name", takeChars(30));
+        setField("cate_type", takeChars(1));
+        setField("tiploc", takeChars(7));
+        setField("subsidiary_3_alpha", takeChars(3));
+        skipChars(3);
+        setField("3_alpha", takeChars(3));
+        setField("easting", takeChars(5));
+        setField("estimated", takeChars(1));
+        setField("northing", takeChars(5));
+        setField("change_time", takeChars(2));
+
+        // The remaining fields (cate footnote, subsector code) are marked as historic
+        // and not maintained, and are ignored as per the specification.
+    }
+}
---
+++ tests/com/kristianhentschel/transportexp/ingest/utilities/records/UnitedKingdomATOC/MasterStationNamesStationRecordTest.java
@@ -0,0 +1,44 @@
+package com.kristianhentschel.transportexp.ingest.utilities.records.UnitedKingdomATOC;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kristian on 05/08/2015.
+ */
+public class MasterStationNamesStationRecordTest {
+
+    @Test
+    public void testStationRecord() throws Exception {
+        String input = "A    HYNDLAND                      1HYNDLNDHYN   HYN12553 66676 3             530 ";
+        MasterStationNamesStationRecord dut = new MasterStationNamesStationRecord(input);
+
+        assertEquals("A", dut.getField("record_type"));
+        assertEquals("HYNDLAND", dut.getField("station_name").trim());
+        assertEquals("1", dut.getField("cate_type"));
+        assertEquals("HYNDLND", dut.getField("tiploc"));
+        assertEquals("HYN", dut.getField("subsidiary_3_alpha"));
+        assertEquals("HYN", dut.getField("3_alpha"));
+        assertEquals("12553", dut.getField("easting"));
+        assertEquals(" ", dut.getField("estimated"));
+        assertEquals("66676", dut.getField("northing"));
+        assertEquals("3", dut.getField("change_time").trim());
+    }
+
+    public void testStationRecordLongFields() throws Exception {
+        String input = "A___012345678901234567890134567890TIPLOC0S3A___3AL00000E0000015";
+        MasterStationNamesStationRecord dut = new MasterStationNamesStationRecord(input);
+
+        assertEquals("A", dut.getField("record_type"));
+        assertEquals("012345678901234567890123456789", dut.getField("station_name").trim());
+        assertEquals("0", dut.getField("cate_type"));
+        assertEquals("TIPLOC0", dut.getField("tiploc"));
+        assertEquals("S3A", dut.getField("subsidiary_3_alpha"));
+        assertEquals("3AL", dut.getField("3_alpha"));
+        assertEquals("00000", dut.getField("easting"));
+        assertEquals("E", dut.getField("estimated"));
+        assertEquals("00000", dut.getField("northing"));
+        assertEquals("15", dut.getField("change_time").trim());
+    }
+}
