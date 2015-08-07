8290e8384019af23328155a31c058b5832bdeaee
Kristian Hentschel <khentschel@gmail.com>
2015-08-07 10:21:02 +0200

    Add Test stubs for Cif record formats.

---
+++ tests/com/kristianhentschel/transportexp/ingest/records/uk/atoc/cif/CifIntermediateLocationRecordTest.java
@@ -0,0 +1,10 @@
+package com.kristianhentschel.transportexp.ingest.records.uk.atoc.cif;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kristian on 07/08/2015.
+ */
+public class CifIntermediateLocationRecordTest {
+
+}
