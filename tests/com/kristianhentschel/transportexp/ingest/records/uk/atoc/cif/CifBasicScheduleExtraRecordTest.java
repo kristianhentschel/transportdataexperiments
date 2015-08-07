4181a8f66ef32a38802bc4bd7852fb7acb2c1361
Kristian Hentschel <khentschel@gmail.com>
2015-08-07 18:06:39 +0200

    Implements basic schedule extra record.

--- tests/com/kristianhentschel/transportexp/ingest/records/uk/atoc/cif/CifBasicScheduleExtraRecordTest.java
+++ tests/com/kristianhentschel/transportexp/ingest/records/uk/atoc/cif/CifBasicScheduleExtraRecordTest.java
@@ -1,10 +1,26 @@
 package com.kristianhentschel.transportexp.ingest.records.uk.atoc.cif;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Kristian on 07/08/2015.
  */
 public class CifBasicScheduleExtraRecordTest {
+    @Test
+    public void testWithRealData() {
+        String input ="BX         GWYGW010300                                                    ";
+        CifBasicScheduleExtraRecord dut = new CifBasicScheduleExtraRecord(input);
+
+        assertEquals(false, dut.hasUicCode());
+        assertEquals("", dut.getUicCode());
+        assertEquals("GW", dut.getAtocCode());
+        assertEquals(true, dut.getAts());
+        assertEquals("GW", dut.getRsidToc());
+        assertEquals("0103", dut.getRsidTrainNumber());
+        assertEquals("00", dut.getRsidPortionNumber());
+
+    }
 
 }
