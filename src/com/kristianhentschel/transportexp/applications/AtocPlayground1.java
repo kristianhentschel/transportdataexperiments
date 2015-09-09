253462253fdbb9218cb623e75f01d2319ed8ac65
Kristian Hentschel <khentschel@gmail.com>
2015-09-09 21:33:25 +0200

    Print service details for a stop in playground app.

--- src/com/kristianhentschel/transportexp/applications/AtocPlayground1.java
+++ src/com/kristianhentschel/transportexp/applications/AtocPlayground1.java
@@ -4,7 +4,9 @@ import com.kristianhentschel.transportexp.ingest.uk.atoc.AtocParser;
 import com.kristianhentschel.transportexp.timetable.TimetableSystem;
 import com.kristianhentschel.transportexp.timetable.records.TimetableFixedLink;
 import com.kristianhentschel.transportexp.timetable.records.TimetableService;
+import com.kristianhentschel.transportexp.timetable.records.TimetableServiceStop;
 import com.kristianhentschel.transportexp.timetable.records.TimetableStop;
+import com.kristianhentschel.transportexp.timetable.utilities.TimetableTimeOfDay;
 
 import java.util.Iterator;
 import java.util.Scanner;
@@ -25,6 +27,9 @@ public class AtocPlayground1 {
         // Now, do something with this data
         Scanner inputScanner = new Scanner(System.in);
 
+        boolean printServices = true;
+        boolean printFixedLinks = false;
+
         while(inputScanner.hasNext()) {
             String request = inputScanner.next();
             if (request.equals("q")) {
@@ -33,24 +38,51 @@ public class AtocPlayground1 {
             }
 
             if (ts.hasStop(request))
-                printStopInfo(ts.getStop(request));
+                printStopInfo(ts.getStop(request), printFixedLinks, printServices);
             else
                 System.out.println("No such stop, " + request);
         }
 
     }
 
-    public static void printStopInfo(TimetableStop stop) {
+    public static void printStopInfo(TimetableStop stop, boolean printLinks, boolean printServices) {
         System.out.println(stop.getNumStoppingServices() + " services stop at " + stop.getName());
 
         // Fixed Links
-        Iterator<TimetableFixedLink> fixedLinkIterator = stop.getFixedLinkIterator();
-        while(fixedLinkIterator.hasNext()) {
-            TimetableFixedLink link = fixedLinkIterator.next();
-            System.out.println( link.getMode() + " to " + link.getDestination().getName() + " in " + link.getDuration());
+        if (printLinks) {
+            Iterator<TimetableFixedLink> fixedLinkIterator = stop.getFixedLinkIterator();
+            while (fixedLinkIterator.hasNext()) {
+                TimetableFixedLink link = fixedLinkIterator.next();
+                System.out.println(link.getMode() + " to " + link.getDestination().getName() + " in " + link.getDuration());
+            }
         }
 
         // Services
-        Iterator<TimetableService> serviceIterator = stop.getServiceIterator();
+        if(printServices) {
+            Iterator<TimetableService> serviceIterator = stop.getServiceIterator();
+            while (serviceIterator.hasNext()) {
+                TimetableService service = serviceIterator.next();
+                Iterator<TimetableServiceStop> scheduleIterator = service.getScheduleIterator();
+                TimetableServiceStop first = null;
+                TimetableServiceStop current = null;
+                TimetableServiceStop last = null;
+                while(scheduleIterator.hasNext()) {
+                    TimetableServiceStop serviceStop = scheduleIterator.next();
+
+                    if(first == null) {
+                        first = serviceStop;
+                    }
+
+                    if(serviceStop.getStop().equals(stop)) {
+                        current = serviceStop;
+                    }
+
+                    last = serviceStop;
+                }
+
+                TimetableTimeOfDay time = current.getDeparts() != null ? current.getDeparts() : current.getArrives();
+                System.out.printf("%s: %s service from %s to %s at %s\n", service.getName(), service.getOperator(), first.getStop().getName(), last.getStop().getName(), time);
+            }
+        }
     }
 }
