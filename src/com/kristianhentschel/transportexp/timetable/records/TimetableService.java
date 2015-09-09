e8e09890723f160b7fdf8826144b07b80e6d5503
Kristian Hentschel <khentschel@gmail.com>
2015-09-09 08:56:44 +0200

    Add constructor and auto-generated getters and setters to timetbale service

--- src/com/kristianhentschel/transportexp/timetable/records/TimetableService.java
+++ src/com/kristianhentschel/transportexp/timetable/records/TimetableService.java
@@ -4,6 +4,7 @@ import com.kristianhentschel.transportexp.timetable.utilities.TimetableDate;
 import com.kristianhentschel.transportexp.timetable.utilities.TimetableDaysOfWeek;
 import com.kristianhentschel.transportexp.timetable.utilities.TimetableTimeOfDay;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,8 +19,52 @@ public class TimetableService extends TimetableRecord {
     private TimetableDate startDate;
     private TimetableDate endDate;
 
-    // stops with arrival and/or departure times
+    // stops with arrival and/or departure times, in service order.
     private List <TimetableServiceStop> schedule;
 
     private TimetableDaysOfWeek daysOfWeek;
+
+    public TimetableService() {
+        schedule = new ArrayList<TimetableServiceStop>();
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TimetableDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(TimetableDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public TimetableDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(TimetableDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public TimetableDaysOfWeek getDaysOfWeek() {
+        return daysOfWeek;
+    }
+
+    public void setDaysOfWeek(TimetableDaysOfWeek daysOfWeek) {
+        this.daysOfWeek = daysOfWeek;
+    }
 }
