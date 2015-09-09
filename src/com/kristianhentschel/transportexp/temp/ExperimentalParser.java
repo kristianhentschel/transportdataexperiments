f393adc3caa88e8e889469a1b9b7a0bd0135f78b
Kristian Hentschel <khentschel@gmail.com>
2015-09-09 11:55:17 +0200

    Implementing fixed links file parsing. Some trouble with the alternative names and keeping track of them, to be refactored. (It works now but it is ugly).

--- src/com/kristianhentschel/transportexp/temp/ExperimentalParser.java
+++ src/com/kristianhentschel/transportexp/temp/ExperimentalParser.java
@@ -1,7 +1,9 @@
 package com.kristianhentschel.transportexp.temp;
 
+import com.kristianhentschel.transportexp.ingest.records.uk.atoc.flf.FixedLinksRecord;
 import com.kristianhentschel.transportexp.ingest.records.uk.atoc.msn.MasterStationNamesStationRecord;
 import com.kristianhentschel.transportexp.timetable.TimetableSystem;
+import com.kristianhentschel.transportexp.timetable.records.TimetableFixedLink;
 import com.kristianhentschel.transportexp.timetable.records.TimetableRecord;
 import com.kristianhentschel.transportexp.timetable.records.TimetableStop;
 import com.kristianhentschel.transportexp.timetable.utilities.TimetableDuration;
@@ -39,34 +41,114 @@ public class ExperimentalParser {
         // initialise the system
         TimetableSystem ts = new TimetableSystem("ATOC", TimetableRecord.DATA_SOURCE.UK_ATOC);
 
+        // alternative station codes still used in these files but not relevant to timetable system
+        Map<String,String> subsidiaryToPrimary = new HashMap<String, String>();
+
         // parse master station names file
-        Map<String, String> tiplocToStationCode = parseMasterStationNames(ts, file_msn);
+        Map<String, String> tiplocToStationCode = parseMasterStationNames(ts, file_msn, subsidiaryToPrimary);
 
         // TODO parse other files using this base data
 
         // parse fixed links file
+        parseFixedLinks(ts, subsidiaryToPrimary, file_flf);
+
+        //parseAdditionalFixedLinks(ts, file_alf);
 
         // To see if it works, print stations from timetable system
+
         Iterator<TimetableStop> it = ts.getStopsIterator();
         while(it.hasNext()) {
             TimetableStop stop = it.next();
-            System.out.println(stop.getName());
+            System.out.print(stop.getName());
+
+            Iterator<TimetableFixedLink> fl_it = stop.getFixedLinkIterator();
+            if(fl_it.hasNext())
+                System.out.print("\t\tFixed links: ");
+            while(fl_it.hasNext()) {
+                TimetableFixedLink fl = fl_it.next();
+                System.out.print(fl.getMode());
+                System.out.print(" to ");
+                if(fl.getOrigin()
+                        .getLocalStopId()
+                        .equals(stop.getLocalStopId())) {
+                    System.out.print(fl.getDestination().getName());
+                } else {
+                    System.out.print(fl.getOrigin().getName());
+                }
+                System.out.print(" ");
+            }
+
+            System.out.println();
         }
+    }
+
+    private static void parseFixedLinks(TimetableSystem ts, Map<String, String> subsidiaryToPrimary, File file_flf) {
+        Scanner sc;
+
+        try {
+            sc = new Scanner(file_flf);
+
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (line.startsWith("END"))
+                    break;
+
+                FixedLinksRecord r = new FixedLinksRecord(line);
+                parseFixedLinksRecord(ts, subsidiaryToPrimary, r);
+            }
+
+        } catch(FileNotFoundException e) {
+            System.out.println("Error opening FLF file: " + e.getMessage());
+        }
+    }
+
+    private static TimetableStop getStop(TimetableSystem ts, Map<String, String> subsidiaryToPrimary, String stopId) {
+        if (subsidiaryToPrimary.containsKey(stopId))
+            return ts.getStop(subsidiaryToPrimary.get(stopId));
+
+        return ts.getStop(stopId);
+    }
+
+    /**
+     * Parses a single fixed links record, adding the resulting fixed link object to both the origin and destination
+     * stop in the timetable system of reference.
+     * @param ts timetable system
+     * @param r record
+     */
+    private static void parseFixedLinksRecord(TimetableSystem ts, Map<String, String> subsidiaryToPrimary, FixedLinksRecord r) {
+
+        TimetableStop origin = getStop(ts, subsidiaryToPrimary, r.getOrigin());
+        TimetableStop destination = getStop(ts, subsidiaryToPrimary, r.getDestination());
+        TimetableFixedLink fl = new TimetableFixedLink();
+
+        fl.setOrigin(origin);
+        fl.setDestination(destination);
+
+        fl.setMode(r.getMode().toString());
+
+        TimetableDuration duration = new TimetableDuration();
+        duration.addMinutes(r.getTime());
+        fl.setDuration(duration);
+
+        // TODO: bi-directional link represented by a uni-directional class added to both...???
+        origin.addFixedLink(fl);
+        destination.addFixedLink(fl);
     }
 
     /**
      * Parses the Master Station Names file containing mappings between tiploc codes and station names, locations, etc.
      * @param ts the timetable system of reference, wherein the stops are created.
-     * @param f A File object describing the msn file path.
+     * @param file_msn A File object describing the msn file path.
+     * @param subsidiaryToPrimary
      * @return A map from tiploc codes to three letter station codes. Useful to determine if a tiploc is a real station.
      */
-    private static Map<String, String> parseMasterStationNames(TimetableSystem ts, File f) {
+    private static Map<String, String> parseMasterStationNames(TimetableSystem ts, File file_msn, Map<String, String> subsidiaryToPrimary) {
         Map<String, String> tiplocToCode = new HashMap<String, String>();
 
         Scanner sc;
 
         try {
-            sc = new Scanner(f);
+            sc = new Scanner(file_msn);
 
             // ignore header record (also starts with A)
             sc.nextLine();
@@ -81,7 +163,7 @@ public class ExperimentalParser {
                 }
 
                 MasterStationNamesStationRecord r = new MasterStationNamesStationRecord(line);
-                parseStop(ts, tiplocToCode, r);
+                parseStop(ts, tiplocToCode, subsidiaryToPrimary, r);
             }
         } catch(FileNotFoundException e) {
             System.out.println("Error opening MSN file: " + e.getMessage());
@@ -90,13 +172,18 @@ public class ExperimentalParser {
         return tiplocToCode;
     }
 
-    private static void parseStop(TimetableSystem ts, Map<String, String> tiplocToCode, MasterStationNamesStationRecord r) {
+    private static void parseStop(TimetableSystem ts, Map<String, String> tiplocToCode, Map<String, String> subsidiaryToPrimary, MasterStationNamesStationRecord r) {
         // Set tiploc to three-letter-code mapping
         tiplocToCode.put(r.getTiploc(), r.getCode());
 
         if (r.isSubsidiary()) {
             // Ignore data other than tiploc-code mapping for subsidiaries!
+            //System.out.println("Ignoring subsidiary with codes: "+r.getCode()+" " +r.getSubsidiaryCode());
             return;
+        } else if (!r.getSubsidiaryCode().equals(r.getCode())) {
+            // for stations with multiple 3-alpha codes, keep track of the equivalences
+            //System.out.println("different codes but not a subsidiary. principal: "+r.getCode()+" subsid: " +r.getSubsidiaryCode());
+            subsidiaryToPrimary.put(r.getSubsidiaryCode(), r.getCode());
         }
 
         // Create stop object in timetable system
